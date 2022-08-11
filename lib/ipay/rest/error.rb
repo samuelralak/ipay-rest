24d5729b6e193e653a247453bc8dabdc26871c16
Samuel Ralak <thesamuelralak@gmail.com>
2022-08-12 00:04:37 +0300

    handling responses from ipay

--- lib/ipay/rest/error.rb
+++ lib/ipay/rest/error.rb
@@ -1,5 +1,35 @@
 module Ipay
   module Rest
     class Error < StandardError; end
+
+    class FailedTransactionError < StandardError
+      def to_s
+        'Failed transaction. Not all parameters fulfilled. A notification of this transaction sent to the merchant.'
+      end
+    end
+
+    class PendingTransactionError < StandardError
+      def to_s
+        'Pending: Incoming Mobile Money Transaction Not found. Please try again in 5 minutes.'
+      end
+    end
+
+    class UsedCodeError < StandardError
+      def to_s
+        'Used: This code has been used already. A notification of this transaction sent to the merchant.'
+      end
+    end
+
+    class PaymentDeficitError < StandardError
+      def to_s
+        'Less: The amount that you have sent via mobile money is LESS than what was required to validate this transaction.'
+      end
+    end
+
+    class OverPaymentError < StandardError
+      def to_s
+        'More: The amount that you have sent via mobile money is MORE than what was required to validate this transaction.'
+      end
+    end
   end
 end
---
+++ lib/ipay/rest/resource.rb
@@ -0,0 +1,49 @@
+require 'openssl'
+
+module Ipay
+  module Rest
+    class Resource
+      attr_reader :client
+
+      def initialize(client)
+        @client = client
+      end
+
+      def post(url, body:, headers: {})
+        body[:hash] = create_hash(body.values.join(""))
+        handle_response client.connection.post(url, body, default_headers.merge(headers))
+      end
+
+      private
+
+      def default_headers
+        {}
+      end
+
+      def handle_response(response)
+        status = response.body['status'] || response.body['header_status']
+
+        case status
+        when 0
+          raise Error, response.body['text']
+        when "fe2707etr5s4wq"
+          raise FailedTransactionError
+        when "bdi6p2yy76etrs"
+          raise PendingTransactionError
+        when "cr5i3pgy9867e1"
+          raise UsedCodeError
+        when "dtfi4p7yty45wq"
+          raise PaymentDeficitError
+        when "eq3i7p5yt7645e"
+          raise OverPaymentError
+        end
+
+        response
+      end
+
+      def create_hash(data_string)
+        OpenSSL::HMAC.hexdigest('sha256', Ipay::Rest::SECRET_KEY, data_string)
+      end
+    end
+  end
+end
